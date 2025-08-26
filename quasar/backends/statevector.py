eb93c221dc91910b83a2551e6e4b9a02ba17cc99
Tim Littau <113526482+tmlittau@users.noreply.github.com>
2025-08-26 05:08:09 +0200

    Add execution scheduler with backend coordination

--- quasar/backends/statevector.py
+++ quasar/backends/statevector.py
@@ -34,6 +34,7 @@ class StatevectorBackend(Backend):
         "H": 1 / np.sqrt(2) * np.array([[1, 1], [1, -1]], dtype=complex),
         "S": np.array([[1, 0], [0, 1j]], dtype=complex),
         "SDG": np.array([[1, 0], [0, -1j]], dtype=complex),
+        "T": np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]], dtype=complex),
         "CX": np.array(
             [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]],
             dtype=complex,
